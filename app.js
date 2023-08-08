8b225234d5914ef123fb4d7a05f5b76fe7343708
m-ole0 <138345360+m-ole0@users.noreply.github.com>
2023-08-08 22:53:31 +0900

    [Start]JavaScriptカリキュラム1&2

---
+++ app.js
@@ -0,0 +1,14 @@
+//let〇〇 = 数値もしくは"文字列"で変数を作れる。
+let hello = "Hello World";
+let int1 = 1;
+
+alert(hello);
+alert(int1);
+alert(4 + 3);
+
+alert("Hello" + "World");
+
+let str1 = "Hello";
+let str2 = "World!!";
+
+alert(str1 + str2);
