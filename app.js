b35f8ce712d2181f73060d7ca099f312b8a2a5d4
m-ole0 <138345360+m-ole0@users.noreply.github.com>
2023-08-08 23:16:30 +0900

    [Finish]JavaScriptカリキュラム2

--- app.js
+++ app.js
@@ -1,14 +1,61 @@
 //let〇〇 = 数値もしくは"文字列"で変数を作れる。
-let hello = "Hello World";
-let int1 = 1;
+//let hello = "Hello World";
+//let int1 = 1;
 
-alert(hello);
-alert(int1);
-alert(4 + 3);
+//変数の使い方
+//alert(hello);
+//alert(int1);
+//alert(4 + 3);
 
-alert("Hello" + "World");
+//alert("Hello" + "World");
 
-let str1 = "Hello";
-let str2 = "World!!";
+//変数は下記のように記載する事で、合体させる事ができる。
+//let str1 = "Hello";
+//let str2 = "World!!";
 
-alert(str1 + str2);
+//alert(str1 + str2);
+
+//ifを使った条件式
+//let orange = 100;
+//let apple = 100;
+
+//if (orange < apple){
+//    alert("みかんの値段がりんごより安い");
+//} else if (orange == apple){
+//    alert("みかんとりんごが同じ値段");
+//} else {
+//    alert("みかんの値段がりんごより高い");
+//}
+
+//繰り返し処置（while文とfor文）
+
+//while文(条件式が真である間は繰り返し処理が実行される)
+//let max = 100;
+//let num = 1;
+//let count = 0;
+
+//while (num < max){
+//    num *= 2;
+//    count += 1;
+//}
+
+//alert("2を掛け続けて" + max + "を超えるのに必要だった回数は"　+ count +"回です");
+
+//for文（決められた回数の処理を繰り返す時に使われる）
+//for (初期値;条件式;増減値){
+//    繰り返し処理:
+//}
+
+//let i;
+//let num = 0;
+
+//for (i=1; i <= 10; i++){
+//    num += i;
+//}
+
+//alert("1から10まで足し算した結果は" + num +"です");
+
+alert(1 + 1);
+alert(1 - 1);
+alert(2 * 3);
+alert(30 / 3);
